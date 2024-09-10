c419811a76b352b864ab3f7951c530542ce9e0d4
sungongtai <1278571731@qq.com>
2024-09-10 15:09:25 +0800

    init 9.10

--- model/src/main/java/com/atguigu/lease/model/entity/BaseEntity.java
+++ model/src/main/java/com/atguigu/lease/model/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.atguigu.lease.model.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -19,12 +16,12 @@ public class BaseEntity implements Serializable {
     private Long id;
 
     @Schema(description = "创建时间")
-    @TableField(value = "create_time")
+    @TableField(value = "create_time", fill = FieldFill.INSERT)
     @JsonIgnore
     private Date createTime;
 
     @Schema(description = "更新时间")
-    @TableField(value = "update_time")
+    @TableField(value = "update_time", fill = FieldFill.UPDATE)
     @JsonIgnore
     private Date updateTime;
 
