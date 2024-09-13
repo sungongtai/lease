a35f7432342b35c428c6becf891f978d38904098
sungongtai <1278571731@qq.com>
2024-09-13 10:54:01 +0800

    实现全局异常处理

---
+++ common/src/main/java/com/atguigu/lease/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.atguigu.lease.common.exception;
+
+import com.atguigu.lease.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result handle(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+}
